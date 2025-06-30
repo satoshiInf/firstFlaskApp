bed805d77b526102bb790e9b2779321aaf8172ca
Satoshi <informationcompetitionsatoshi@gmail.com>
2025-06-30 16:54:06 +0900

    made 3 pages. TOP, LIST, DETAIL

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
-
+# routing 
 @app.route("/")
-def hello():
-    return "Hello, Flask！!"
+def index():
+    return render_template('top.html')
+
+@app.route("/list")
+def list():
+    return render_template('list.html')
+
+@app.route("/detail")
+def detail():
+    return render_template('detail.html')
+
 
 if __name__ == '__main__':
     app.run()
